164c53c0ba5971d4163dd635c536f09881bf9600
Piotr Obara <pobara@o2.pl>
2023-12-15 18:08:44 +0100

    step 2

--- src/components/TaskskList.js
+++ src/components/TaskskList.js
@@ -2,59 +2,56 @@ import { useRef, useState } from 'react';
 import { tasks as tasksData } from '../data/tasks';
 
 export function TasksList() {
-	const [tasks, setTasks] = useState(tasksData);
+    const [tasks, setTasks] = useState(tasksData);
 
     const titleRef = useRef(null);
 
-	const handleAddTask = () => {
-		const newTasks = [...tasks];
-		newTasks.push({
-			title: titleRef.current.value,
-			description: "wash the dishes in the kitchen",
-			completed: false,
-		});
-		setTasks(newTasks);
-	};
+    const handleAddTask = () => {
+        const newTasks = [...tasks];
+        newTasks.push({
+            title: titleRef.current.value,
+            description: "wash the dishes in the kitchen",
+            completed: false,
+        });
+        setTasks(newTasks);
+        titleRef.current.value = ''; // Wyczyszczenie pola tekstowego po dodaniu zadania
+    };
 
-	const handleToggleTaskState = (index) => {
-		const newTasks = [...tasks];
-		newTasks[index].completed = !newTasks[index].completed;
-		setTasks(newTasks);
-	};
+    const handleToggleTaskState = (index) => {
+        const newTasks = [...tasks];
+        newTasks[index].completed = !newTasks[index].completed;
+        setTasks(newTasks);
+    };
 
-	const handleDeleteTask = (index) => {
-		const newTasks = [...tasks];
-		newTasks.splice(index, 1);
-		setTasks(newTasks);
-	};
+    const handleDeleteTask = (index) => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    };
 
-    if (tasks.length === 0) return <div>Empty task list.</div>
+    return (
+        <>
+            <input type="text" id="title" ref={titleRef} onKeyPress={(e) => e.key === 'Enter' && handleAddTask()} />
+            <button onClick={handleAddTask}>Add new task</button>
 
-	return (
-		<>
-
-            <input type="text" id="title" ref={titleRef}/>
-			<button onClick={handleAddTask}>Add new task</button>
-
-            {tasks.length === 0 ? (<div>Empty task list.</div>) : (
-           	<ul>
-               {tasks.map(({ title, completed }, index) => (
-                       
-                           <li
-                               key={index}
-                               style={{ textDecoration: completed ? 'line-through' : 'none' }}
-                           >
-                               {title}
-                               <button onClick={() =>  handleToggleTaskState(index)}>{completed ? 'Undo' : "completed"}</button>
-                               <button onClick={() => handleDeleteTask(index)}>Delete</button>
-                           </li>
-                       
-                   ))};
-               
-           </ul>
+            {tasks.length === 0 ? (
+                <div>No tasks available.</div>
+            ) : (
+                <ul>
+                    {tasks.map(({ title, description, completed }, index) => (
+                        <li
+                            key={index}
+                            style={{ textDecoration: completed ? 'line-through' : 'none' }}
+                        >
+                            <strong>{title}</strong> - {description.slice(0, 50) + (description.length > 50 ? '...' : '')}
+                            <button onClick={() => handleToggleTaskState(index)}>
+                                {completed ? 'Undo' : "Complete"}
+                            </button>
+                            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
             )}
-
-		
-		</>
-	);
+        </>
+    );
 }
